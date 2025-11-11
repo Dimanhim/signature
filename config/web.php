39205af5343b932c3f14a4d41eb565e9a3d0e83f
Dimanhim <dimanhim@list.ru>
2025-11-11 10:53:13 +0300

    #36340 - бесконечная авторизация v2

--- config/web.php
+++ config/web.php
@@ -33,6 +33,7 @@ $config = [
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
+            'authTimeout' => 3600 * 24 * 30
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
